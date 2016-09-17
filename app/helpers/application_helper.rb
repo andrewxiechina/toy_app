535ca7311c2e9440f876da08fdb9a840e1536a4c
Andrew Xie <andrewcrab.china@gmail.com>
2016-09-16 23:21:30 -0700

    Add dynamics to static_pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def translateToTitle(title = "")
+    if title.empty?
+      return "Andrew Xie - The Pro Problem Solver"
+    else
+      return title + " | " + "Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base =  " | Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home" + @base
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help" + @base
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About" + @base
   end
 
   test "should get projects" do
     get static_pages_projects_url
     assert_response :success
+    assert_select "title", "Andrew Xie - The Pro Problem Solver"
   end
 
 end
