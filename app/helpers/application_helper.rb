0a05f462f7cc9722a0be6242025d6cccc6494b86
Andrew Xie <andrewcrab.china@gmail.com>
2016-09-18 14:56:10 -0700

    clean all

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  def translateToTitle(title = "")
-    if title.empty?
-      return "Andrew Xie - The Pro Problem Solver"
-    else
-      return title + " | " + "Ruby on Rails Tutorial Sample App"
-    end
-  end
-end
