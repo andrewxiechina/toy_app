14378ff969844d56baf61b53bf6e5c5ca1a3cab3
Andrew Xie <andrewcrab.china@gmail.com>
2016-09-18 15:24:00 -0700

    Add danymic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def s_to_title(title="")
+    if title.empty?
+      return "Ruby on Rails Tutorial Sample App"
+    else
+      return title + " | Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
